68ffc507e11b0031c1e240de6b7a8189b25bcc3d
tashisrael <tashisrael04@gmail.com>
2023-03-07 19:39:30 +0300

    fix linter errors

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,8 +1,10 @@
-class Api::GreetingsController < ApplicationController
-  def index
-    count_greetings = Greeting.count
-    random_offset = rand(count_greetings)
-    greetings = Greeting.offset(random_offset).first
-    render json: greetings
+module Api
+  class GreetingsController < ApplicationController
+    def index
+      count_greetings = Greeting.count
+      random_offset = rand(count_greetings)
+      greetings = Greeting.offset(random_offset).first
+      render json: greetings
+    end
   end
 end
